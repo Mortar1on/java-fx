2d09098429a388e9b05ead0730742ab637a5796b
denys <kapralov>
2022-01-26 20:23:45 +0200

    first m

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module ua.ithillel.javafx {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.kordamp.bootstrapfx.core;
+
+    opens ua.ithillel.javafx to javafx.fxml;
+    exports ua.ithillel.javafx;
+}
